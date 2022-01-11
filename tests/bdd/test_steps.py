53c626cdb1d2eb1d258e389c61ea1d4d93162221
Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-01-11 11:24:06 +0100

    Feature/CEDT-4 Add "with_type" step for BDD Behave integration.

--- tests/bdd/test_steps.py
+++ tests/bdd/test_steps.py
@@ -10,7 +10,7 @@ from connect.devops_testing.bdd.steps import (
     with_parameter_not_checked, with_parameter_without_value, with_parameter_without_value_error,
     with_asset_tier_customer, with_asset_tier_tier1, with_asset_tier_tier2, with_connection_id, with_item_quantity,
     with_items, request_note_is, request_reason_is, with_note, with_reason, with_asset_tier_from_country,
-    with_asset_external_id, with_asset_external_uid,
+    with_asset_external_id, with_asset_external_uid, with_type,
 )
 
 PARAM_ID_A = 'PARAM_ID_A'
@@ -50,6 +50,7 @@ def test_step_should_create_a_tier_configuration_request(behave_context):
 
     tier_config_request(behave_context)
     with_id(behave_context, 'TCR-000-000-000-000')
+    with_type(behave_context, 'setup')
     with_note(behave_context, NOTE)
     with_reason(behave_context, REASON)
     with_status(behave_context, 'pending')
@@ -57,7 +58,7 @@ def test_step_should_create_a_tier_configuration_request(behave_context):
     with_marketplace_id(behave_context, 'MP-00000')
     with_connection_id(behave_context, 'CT-0000-0000-0000', 'test')
     with_tier_config_account(behave_context, 'TA-0000-0000-0000')
-    with_reseller_level(behave_context, 2)
+    with_reseller_level(behave_context, "2")
     with_parameter_with_value(behave_context, PARAM_ID_A, PARAM_ID_A_VALUE)
     with_parameter_with_value_error(behave_context, PARAM_ID_A, PARAM_ID_A_VALUE_ERROR)
     with_parameter_without_value(behave_context, PARAM_ID_NO_VALUE)
@@ -75,7 +76,7 @@ def test_step_should_create_a_tier_configuration_request(behave_context):
     assert request['configuration']['marketplace']['id'] == 'MP-00000'
     assert request['configuration']['connection']['id'] == 'CT-0000-0000-0000'
     assert request['configuration']['connection']['type'] == 'test'
-    assert request['configuration']['tier_level'] == 2
+    assert request['configuration']['tier_level'] == "2"
     assert request['configuration']['params'][0]['id'] == PARAM_ID_A
     assert request['configuration']['params'][0]['value'] == PARAM_ID_A_VALUE
     assert request['configuration']['params'][0]['value_error'] == PARAM_ID_A_VALUE_ERROR
@@ -116,6 +117,7 @@ def test_step_should_create_an_asset_request(behave_context):
 
     asset_request(behave_context)
     with_id(behave_context, 'PR-000-000-000-000')
+    with_type(behave_context, 'change')
     with_status(behave_context, 'pending')
     with_product_id(behave_context, 'PRD-000-000-000')
     with_note(behave_context, NOTE)
@@ -144,7 +146,7 @@ def test_step_should_create_an_asset_request(behave_context):
 
     assert request['id'] == 'PR-000-000-000-000'
     assert request['status'] == 'pending'
-    assert request['type'] == 'purchase'
+    assert request['type'] == 'change'
     assert request['note'] == NOTE
     assert request['reason'] == REASON
     assert request['asset']['external_id'] == '123456789'
