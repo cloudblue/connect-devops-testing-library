6246df1e5050f0842e196af3508226341d4ec6eb
Unay Santisteban <davidunay.santisteban@cloudblue.com>
2022-01-21 13:18:40 +0100

    Feature/CEDT-7 Asserts improvements. (#27)
    * request_id
    * request_type
    * request_param_value
    * request_param_value_[equal|not_equal|match|contain]
    * request_param_value_error_[equal|not_equal|match|contain]

--- connect/devops_testing/asserts.py
+++ connect/devops_testing/asserts.py
@@ -6,6 +6,15 @@ from connect.devops_testing.utils import find_by_id
 
 ASSERT_FAIL = 'Assertion failed.'
 
+_MSG_PARAM_NOT_EQUAL = "{param_id} parameter value '{value}' is not equal to '{expected}'."
+_MSG_PARAM_IS_EQUAL = "{param_id} parameter value '{value}' is equal to '{expected}'."
+_MSG_PARAM_NOT_MATCH = "{param_id} parameter value '{value}' does not match expression '{expected}'."
+_MSG_PARAM_NOT_CONTAIN = "{param_id} parameter value '{value}' does not contain '{expected}'."
+_MSG_PARAM_ERROR_NOT_EQUAL = "{param_id} parameter value error '{value}' is not equal to '{expected}'."
+_MSG_PARAM_ERROR_IS_EQUAL = "{param_id} parameter value error '{value}' is equal to '{expected}'."
+_MSG_PARAM_ERROR_NOT_MATCH = "{param_id} parameter value error '{value}' does not match '{expected}'."
+_MSG_PARAM_ERROR_NOT_CONTAIN = "{param_id} parameter value error '{value}' does not contain '{expected}'."
+
 __operators = {
     '==': operator.eq,
     '!=': operator.ne,
@@ -21,8 +30,16 @@ def _prepare_assert_argument(param: dict, expected: Any) -> Tuple[Optional[Any],
         return param.get('value', ''), expected
 
 
+def request_id(request: dict, expected: str):
+    assert request.get('id') == expected, f"Request id '{request.get('id')}' is not '{expected}'"
+
+
 def request_status(request: dict, expected: str):
     assert request.get('status') == expected, f"Request status '{request.get('status')}' is not '{expected}'."
+
+
+def request_type(request: dict, expected: str):
+    assert request.get('type') == expected, f"Request type '{request.get('type')}' is not '{expected}'."
 
 
 def request_reason(request: dict, expected: str):
@@ -31,6 +48,60 @@ def request_reason(request: dict, expected: str):
 
 def request_note(request: dict, expected: str):
     assert request.get('note') == expected, f"Request note '{request.get('note')}' is not '{expected}'."
+
+
+def request_param_value(request: dict, param_id: str, operator: str, expected: Any, msg: Optional[str] = None):
+    fn = __operators.get(operator)
+    param = find_by_id(request.get('params', []), param_id, {})
+    value, expected = _prepare_assert_argument(param, expected)
+    assert fn(value, expected), ASSERT_FAIL if msg is None else msg.format(
+        param_id=param_id,
+        value=value,
+        expected=expected,
+    )
+
+
+def request_param_value_equal(request: dict, param_id: str, expected: Any):
+    request_param_value(request, param_id, '==', expected, _MSG_PARAM_NOT_EQUAL)
+
+
+def request_param_value_match(request: dict, param_id: str, pattern: str):
+    request_param_value(request, param_id, 'match', pattern, _MSG_PARAM_NOT_MATCH)
+
+
+def request_param_value_not_equal(request: dict, param_id: str, expected: Any):
+    request_param_value(request, param_id, '!=', expected, _MSG_PARAM_IS_EQUAL)
+
+
+def request_param_value_contains(request: dict, param_id: str, expected: Any):
+    request_param_value(request, param_id, 'in', expected, _MSG_PARAM_NOT_CONTAIN)
+
+
+def request_param_value_error(request: dict, param_id: str, operator: str, expected: Any, msg: Optional[str] = None):
+    fn = __operators.get(operator)
+    param = find_by_id(request.get('params', []), param_id, {})
+    value = param.get('value_error', '')
+    assert fn(value, expected), ASSERT_FAIL if msg is None else msg.format(
+        param_id=param_id,
+        value=value,
+        expected=expected,
+    )
+
+
+def request_param_value_error_equal(request: dict, param_id: str, expected: Any):
+    request_param_value_error(request, param_id, '==', expected, _MSG_PARAM_ERROR_NOT_EQUAL)
+
+
+def request_param_value_error_not_equal(request: dict, param_id: str, expected: Any):
+    request_param_value_error(request, param_id, '!=', expected, _MSG_PARAM_ERROR_IS_EQUAL)
+
+
+def request_param_value_error_contains(request: dict, param_id: str, expected: Any):
+    request_param_value_error(request, param_id, 'in', expected, _MSG_PARAM_ERROR_NOT_CONTAIN)
+
+
+def request_param_value_error_match(request: dict, param_id: str, pattern: str):
+    request_param_value_error(request, param_id, 'match', pattern, _MSG_PARAM_ERROR_NOT_MATCH)
 
 
 def task_response_status(task_response, expected: str):
@@ -54,23 +125,19 @@ def asset_param_value(request: dict, param_id: str, operator: str, expected: Any
 
 
 def asset_param_value_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value '{value}' is not equal to '{expected}'."
-    asset_param_value(request, param_id, '==', expected, msg)
+    asset_param_value(request, param_id, '==', expected, _MSG_PARAM_NOT_EQUAL)
 
 
 def asset_param_value_match(request: dict, param_id: str, pattern: str):
-    msg = "{param_id} parameter value '{value}' does not match expression '{expected}'."
-    asset_param_value(request, param_id, 'match', pattern, msg)
+    asset_param_value(request, param_id, 'match', pattern, _MSG_PARAM_NOT_MATCH)
 
 
 def asset_param_value_not_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value '{value}' is equal to '{expected}'."
-    asset_param_value(request, param_id, '!=', expected, msg)
+    asset_param_value(request, param_id, '!=', expected, _MSG_PARAM_IS_EQUAL)
 
 
 def asset_param_value_contains(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value '{value}' does not contain '{expected}'."
-    asset_param_value(request, param_id, 'in', expected, msg)
+    asset_param_value(request, param_id, 'in', expected, _MSG_PARAM_NOT_CONTAIN)
 
 
 def asset_param_value_error(request: dict, param_id: str, operator: str, expected: Any, msg: Optional[str] = None):
@@ -85,23 +152,19 @@ def asset_param_value_error(request: dict, param_id: str, operator: str, expecte
 
 
 def asset_param_value_error_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value error '{value}' is not equal to '{expected}'."
-    asset_param_value_error(request, param_id, '==', expected, msg)
+    asset_param_value_error(request, param_id, '==', expected, _MSG_PARAM_ERROR_NOT_EQUAL)
 
 
 def asset_param_value_error_not_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value error '{value}' is equal to '{expected}'."
-    asset_param_value_error(request, param_id, '!=', expected, msg)
+    asset_param_value_error(request, param_id, '!=', expected, _MSG_PARAM_ERROR_IS_EQUAL)
 
 
 def asset_param_value_error_contains(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value error '{value}' does not contain '{expected}'."
-    asset_param_value_error(request, param_id, 'in', expected, msg)
+    asset_param_value_error(request, param_id, 'in', expected, _MSG_PARAM_ERROR_NOT_CONTAIN)
 
 
 def asset_param_value_error_match(request: dict, param_id: str, pattern: str):
-    msg = "{param_id} parameter value error '{value}' does not match '{expected}'."
-    asset_param_value_error(request, param_id, 'match', pattern, msg)
+    asset_param_value_error(request, param_id, 'match', pattern, _MSG_PARAM_ERROR_NOT_MATCH)
 
 
 def tier_configuration_status(request: dict, expected: str):
@@ -131,23 +194,19 @@ def tier_configuration_param_value(
 
 
 def tier_configuration_param_value_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value '{value}' is not equal to '{expected}'."
-    tier_configuration_param_value(request, param_id, '==', expected, msg)
+    tier_configuration_param_value(request, param_id, '==', expected, _MSG_PARAM_NOT_EQUAL)
 
 
 def tier_configuration_param_value_not_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value '{value}' is equal to '{expected}'."
-    tier_configuration_param_value(request, param_id, '!=', expected, msg)
+    tier_configuration_param_value(request, param_id, '!=', expected, _MSG_PARAM_IS_EQUAL)
 
 
 def tier_configuration_param_value_contains(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value '{value}' does not contain '{expected}'."
-    tier_configuration_param_value(request, param_id, 'in', expected, msg)
+    tier_configuration_param_value(request, param_id, 'in', expected, _MSG_PARAM_NOT_CONTAIN)
 
 
 def tier_configuration_param_value_match(request: dict, param_id: str, pattern: str):
-    msg = "{param_id} parameter value '{value}' does not match expression '{expected}'."
-    tier_configuration_param_value(request, param_id, 'match', pattern, msg)
+    tier_configuration_param_value(request, param_id, 'match', pattern, _MSG_PARAM_NOT_MATCH)
 
 
 def tier_configuration_param_value_error(
@@ -172,20 +231,16 @@ def tier_configuration_param_value_error(
 
 
 def tier_configuration_param_value_error_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value error '{value}' is not equal to '{expected}'."
-    tier_configuration_param_value_error(request, param_id, '==', expected, msg)
+    tier_configuration_param_value_error(request, param_id, '==', expected, _MSG_PARAM_ERROR_NOT_EQUAL)
 
 
 def tier_configuration_param_value_error_not_equal(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value error '{value}' is equal to '{expected}'."
-    tier_configuration_param_value_error(request, param_id, '!=', expected, msg)
+    tier_configuration_param_value_error(request, param_id, '!=', expected, _MSG_PARAM_ERROR_IS_EQUAL)
 
 
 def tier_configuration_param_value_error_contains(request: dict, param_id: str, expected: Any):
-    msg = "{param_id} parameter value error '{value}' does not contain '{expected}'."
-    tier_configuration_param_value_error(request, param_id, 'in', expected, msg)
+    tier_configuration_param_value_error(request, param_id, 'in', expected, _MSG_PARAM_ERROR_NOT_CONTAIN)
 
 
 def tier_configuration_param_value_error_match(request: dict, param_id: str, pattern: str):
-    msg = "{param_id} parameter value error '{value}' does not match '{expected}'."
-    tier_configuration_param_value_error(request, param_id, 'match', pattern, msg)
+    tier_configuration_param_value_error(request, param_id, 'match', pattern, _MSG_PARAM_ERROR_NOT_MATCH)
